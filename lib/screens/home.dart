e639316bbb9b71c7543c4dec39e768966b128f9a
Sagar Choudhary <sagarchoudhary15947@gmail.com>
2025-07-03 08:20:27 +0530

    pushing intial setup for expense tracker app

---
+++ lib/screens/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: SizedBox.expand(), // Fills the screen with empty space
+    );
+  }
+}
