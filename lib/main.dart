cdab9d65a413cbad152011bc657cde9044362872
Sagar Choudhary <sagarchoudhary15947@gmail.com>
2025-07-17 08:08:50 +0530

    uisng correct screen on entering the main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'screens/login_screen_v1/login_screen.dart';
-import 'screens/login_screen_v1/register_screen.dart';
 import 'utiility/device_info.dart';
 
 void main() {
