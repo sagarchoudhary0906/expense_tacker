8b9cc5e1ac1653dd48928931da777e857da7afb4
Sagar Choudhary <sagarchoudhary15947@gmail.com>
2025-07-18 06:48:36 +0530

    intial firebase inetgration setup with the application for firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 import 'screens/login_screen_v1/login_screen.dart';
 import 'utiility/device_info.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
